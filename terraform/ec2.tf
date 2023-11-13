749d4fcb1f07794352bf32e61578908f4bb18dca
Adnan Alshar <adnanalshare92@gmail.com>
2023-11-13 23:23:18 +0100

    fix tf plan

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "webserver_instance" {
   instance_type               = "t3.micro"
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.webserver_sg.id]
-  subnet_id                   = module.vpc.public_subnets[*]
+  subnet_id                   = module.vpc.public_subnets.*.id
   user_data                   = file("user_data.sh")
   tags = {
     Name = "webserver"
